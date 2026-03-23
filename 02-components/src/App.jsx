6e2fd173aaffa069a4e74b930ef91e0527832011
Shreya Wani <shreyawani2006@gmail.com>
2026-03-23 14:50:14 +0530

    learning about components

---
+++ 02-components/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Card from './components/Card'
+import Navigation from './components/Navigation'
+
+const App = () => {
+  return (
+    <div>
+      <Navigation />
+      <Card />
+    </div>
+  )
+}
+
+export default App
---
+++ 02-components/src/components/Card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Card = () => {
+    return (
+        <div>
+            <div className="card">
+                <h1>Nobita Please Bachaoooo....</h1>
+                <p1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa quae, optio qui fuga explicabo, adipisci dolorem omnis consectetur officiis ratione dolores nostrum quisquam quia architecto cumque eum ullam earum aperiam!</p1>
+            </div>
+        </div>
+    )
+}
+
+export default Card
