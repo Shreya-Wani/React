bddef617d86d3807b301f27ddb74759bee01342c
Shreya Wani <shreyawani2006@gmail.com>
2026-03-25 12:42:04 +0530

    learn to use talwind css setup and use

---
+++ 06-talwind/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-gray-800 h-screen flex items-center justify-center'>
+      
+    </div>
+  )
+}
+
+export default App
